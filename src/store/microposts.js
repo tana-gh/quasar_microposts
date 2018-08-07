2cebd9f829862550d3b00930bc3f9db9c55eba6c
tana_bb <yozoranotakaku@gmail.com>
2018-08-08 00:06:21 +0900

    feat: microposts store

--- src/store/microposts.js
+++ src/store/microposts.js
@@ -2,15 +2,51 @@ import axios  from 'axios'
 import * as C from '../constants'
 
 const state = {
-    microposts: []
+    microposts: {}
 }
 
 const mutations = {
+    getMicroposts(state, { microposts }) {
+        Object.assign(state.microposts, toObject(microposts))
+    },
 
+    postMicroposts(state, { micropost }) {
+        Object.assign(state.microposts, toObject([micropost]))
+    }
 }
 
 const actions = {
+    async getMicroposts({ commit, rootGetters }, { index, count }) {
+        const res = axios.post(
+                C.urlGetMicroposts,
+                { index, count },
+                { headers: rootGetters.authHeader }
+            )
 
+        if (res.data.status) {
+            commit(C.getMicroposts, { microposts: res.data.microposts })
+        }
+    },
+
+    async postMicropost({ commit, rootGetters }, { micropost }) {
+        const res = axios.post(
+                C.urlPostMicropost,
+                { micropost },
+                { headers: rootGetters.authHeader }
+            )
+
+        if (res.data.status) {
+            commit(C.postMicropost, { micropost: res.data.micropost })
+        }
+    }
+}
+
+const toObject = array => {
+    array.reduce((prev, curr) => {
+        const newObj = {}
+        newObj[curr.id] = curr
+        Object.assign(prev, newObj)
+    })
 }
 
 export default {
